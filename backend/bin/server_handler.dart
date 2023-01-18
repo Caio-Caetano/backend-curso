d5dae4636db625581c73b2f0f2c17fef77f2792e
Caio-Caetano <caio.lcaetano@gmail.com>
2023-01-18 16:29:18 -0300

    Json e metodo Post

--- backend/bin/server_handler.dart
+++ backend/bin/server_handler.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 
@@ -8,19 +10,28 @@ class ServeHandler {
       return Response(200, body: 'Primeira Rota');
     });
 
-    // Passar informacao para a rota
-    // o shelf ira tratar e popular o que estiver no meio dos sinais de < >
     router.get('/ola/mundo/<usuario>', (Request req, String usuario) {
       return Response.ok('Ola mundo! $usuario');
     });
 
-    // /query/?nome=Caio
-    // envio eh feito pelo simbolo ? 
-    // e para enviar mais de uma informacao usa-se o &
     router.get('/query', (Request req) {
       String? nome = req.url.queryParameters['nome'];
       String? idade = req.url.queryParameters['idade'];
       return Response.ok('Query eh: nome: $nome, idade: $idade');
+    });
+
+    router.post('/login', (Request req) async {
+      var result = await req.readAsString();
+      Map json = jsonDecode(result);
+
+      var usuario = json['usuario'];
+      var senha = json['senha'];
+
+      if (usuario == 'admin' && senha == '123'){
+        return Response.ok('Bem vindo $usuario');
+      }else{
+        return Response.forbidden('Acesso negado');
+      }
     });
 
     return router;
