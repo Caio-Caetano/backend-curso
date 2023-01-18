2067291827ac3db5070c86f03ef486236e605aa2
Caio-Caetano <caio.lcaetano@gmail.com>
2023-01-18 17:08:39 -0300

    .

--- backend/bin/backend.dart
+++ backend/bin/backend.dart
@@ -5,14 +5,9 @@ import 'api/login_api.dart';
 import 'infra/custom_server.dart';
 
 void main() async {
-  // Varios handlers separadamente, cascade
-  // verificar se o usuario esta verificado para acessar alguma rota, pelo middleware
-
   var cascadeHandler =
       Cascade().add(LoginApi().handler).add(BlogApi().handler).handler;
 
-  // possivel de colocar handlers e middleware
-  // logRequests()
   var handler =
       Pipeline().addMiddleware(logRequests()).addHandler(cascadeHandler);
 
