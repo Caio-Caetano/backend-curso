bb5cb3c16bd748f044927b7ef338dc968c84fc5a
Caio-Caetano <caio.lcaetano@gmail.com>
2023-01-18 16:11:42 -0300

    Passagem de dados com Query

--- backend/bin/server_handler.dart
+++ backend/bin/server_handler.dart
@@ -7,6 +7,22 @@ class ServeHandler {
     router.get('/', (Request request) {
       return Response(200, body: 'Primeira Rota');
     });
+
+    // Passar informacao para a rota
+    // o shelf ira tratar e popular o que estiver no meio dos sinais de < >
+    router.get('/ola/mundo/<usuario>', (Request req, String usuario) {
+      return Response.ok('Ola mundo! $usuario');
+    });
+
+    // /query/?nome=Caio
+    // envio eh feito pelo simbolo ? 
+    // e para enviar mais de uma informacao usa-se o &
+    router.get('/query', (Request req) {
+      String? nome = req.url.queryParameters['nome'];
+      String? idade = req.url.queryParameters['idade'];
+      return Response.ok('Query eh: nome: $nome, idade: $idade');
+    });
+
     return router;
   }
 }
